ba558dfb9a344e7a0385e7d20ae4f0dbbcd13db6
wonknu10 <fabrice.labbe@adfab.fr>
2017-06-08 10:42:24 +0200

    config path template mail

--- routes/get/configuration.js
+++ routes/get/configuration.js
@@ -5,11 +5,14 @@ var path = require('path');
 
 var route = function route(req, res, next, abe) {
   if(req.query.recipient != null){
+    if(typeof abe.config.mail === 'undefined' || abe.config.mail === null) abe.config.mail = {from: 'default@mail.com', recipient: 'recipient@mail.com', subject: 'A subject', "template": "/mail/contact.html"};
+    if(typeof abe.config.captcha === 'undefined' || abe.config.captcha === null) abe.config.captcha = {secret: 'YOUR_RECAPTCHA_SECRET'};
+    if(typeof abe.config.smtp === 'undefined' || abe.config.smtp === null) abe.config.smtp = {"service": "mandrill", options: {auth: {apiKey: 'YOUR_API_KEY'}}}
     abe.config.mail.from = req.query.from;
     abe.config.mail.recipient = req.query.recipient;
-    abe.config.smtp.options.auth.apiKey = req.query.mandrill_api_key;
     abe.config.mail.subject = req.query.subject;
     abe.config.captcha.secret = req.query.captcha;
+    abe.config.smtp.options.auth.apiKey = req.query.mandrill_api_key;
     fs.writeFile(abe.config.root + '/abe.json', JSON.stringify(abe.config), 'utf8');
     res.json({'ok': 'ok'});
     return;
