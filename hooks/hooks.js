ec2115deb23133bfb981525ed727c3790cf1ca69
nicolaslabbe <nicolas.labbe@adfab.fr>
2017-07-24 15:14:05 +0200

    fix user enable

---
+++ hooks/hooks.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var hooks = {
+  afterExpress: function(app, express, abe) {
+    var configUserEnable = abe.config.users.enable
+    app.use('/', function (req, res, next) {
+      if (req.url.indexOf('/abe/plugin/abe-mailer/send') > -1) {
+        abe.config.users.enable = false
+      }
+    next()
+    })
+    var lastBecomeFirst = app._router.stack.pop() 
+    app._router.stack = [lastBecomeFirst].concat(app._router.stack)
+
+    app.use('/', function (req, res, next) {
+      if (req.url.indexOf('/abe/plugin/abe-mailer/send') > -1) {
+        abe.config.users.enable = configUserEnable
+      }
+      next()
+    })
+
+    return app
+  }
+};
+
+exports.default = hooks;
