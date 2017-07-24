effae9b6e854d0a9cbd37393f043eec1c3e405b6
wonknu10 <fabrice.labbe@adfab.fr>
2017-07-24 16:00:48 +0200

    route recaptcha

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -4,16 +4,16 @@ var hooks = {
   afterExpress: function(app, express, abe) {
     var configUserEnable = abe.config.users.enable
     app.use('/', function (req, res, next) {
-      if (req.url.indexOf('/abe/plugin/abe-mailer/send') > -1) {
+      if (req.url.indexOf('/abe/plugin/abe-mailer/send') > -1 || req.url.indexOf('/abe/plugin/abe-mailer/captcha') > -1) {
         abe.config.users.enable = false
       }
     next()
     })
-    var lastBecomeFirst = app._router.stack.pop() 
+    var lastBecomeFirst = app._router.stack.pop()
     app._router.stack = [lastBecomeFirst].concat(app._router.stack)
 
     app.use('/', function (req, res, next) {
-      if (req.url.indexOf('/abe/plugin/abe-mailer/send') > -1) {
+      if (req.url.indexOf('/abe/plugin/abe-mailer/send') > -1 || req.url.indexOf('/abe/plugin/abe-mailer/captcha') > -1) {
         abe.config.users.enable = configUserEnable
       }
       next()
